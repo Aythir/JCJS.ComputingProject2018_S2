ad3fb4183ae71b5c4f1f816e174d9b881f78d97e
Steve Daniel <steve@posmarket.com.au>
2018-07-19 20:23:33 +1000

    Updates to the PHP
    Added modal and navigation

--- JCJS.PrototypeWebsite/adminHeader.php
+++ JCJS.PrototypeWebsite/adminHeader.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php echo $title?></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap core CSS -->
