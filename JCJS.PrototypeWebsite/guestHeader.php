4fd2893817b464c50b5e8e8f1cfe058f4c173f60
Jackson Meyn <jacksonmeyn@gmail.com>
2018-08-27 18:30:49 +1000

    Made temp change to guestHeader because I was getting a too many redirects error message

--- JCJS.PrototypeWebsite/guestHeader.php
+++ JCJS.PrototypeWebsite/guestHeader.php
@@ -2,12 +2,12 @@
     session_start();
 
     // check that user is logged in before proceeding
-    if(isset($_SESSION['EventID'])) {
+    /* if(isset($_SESSION['EventID'])) {
         // proceed as the guest has entered a valid event code
     } else {
         // if user not logged in then redirect to login page
         header("Location: index.php?error=8");
-    }
+    } */
 ?>
 <!DOCTYPE html>
 <html lang="en">
