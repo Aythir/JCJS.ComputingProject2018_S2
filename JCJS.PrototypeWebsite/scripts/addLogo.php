171ee0ee70d102423d58dad5240de566c83728ed
Jackson Meyn <jacksonmeyn@gmail.com>
2018-08-31 09:37:05 +1000

    More work on addLogo.php

--- JCJS.PrototypeWebsite/scripts/addLogo.php
+++ JCJS.PrototypeWebsite/scripts/addLogo.php
@@ -12,63 +12,47 @@
         "api_key" => "831219221732949", 
         "api_secret" => "lNdOEX5stZEosAsWZjv2bkqQlkM" 
       ));
-    //This function will detect and create thumbnails of 200 and 500 width for a given eventId
-    //Assuming the event photos are in ./eventPhotos/$eventId relative to this script
-    //A thumbnail subdirectory will be created in the eventId directory if it doesn't already exist
-    //Thumbnails will not be created if they already exist
-    function detectAndCreateThumbnails(int $eventId) {
-        echo "Script called";
+
+    function addLogoByEventId(int $eventId) {
+
+        //Upload latest version of logo
+        $lrpLogoPath = '../img/logo.png';
+        $lrpLogoUpload = \Cloudinary\Uploader::upload($lrpLogoPath, array('overwrite' => true, 'public_id' => 'lrpLogo'));
+
         $filepath = '../eventPhotos/' . $eventId;
-        $thumbnailPath = $filepath . '/thumbnails';
+        $logoPath = $filepath . '/logo_originals';
         $filepath = realpath($filepath);
+        $logoPath = realpath($logoPath);
 
-        //If thumbnail subdirectory doesn't exist, create it
-        if (!file_exists($thumbnailPath)) {
-            mkdir($thumbnailPath);
+        //If logo subdirectory doesn't exist, create it
+        if (!file_exists($logoPath)) {
+            mkdir($logoPath);
         }
-        $thumbnailPath = realpath($thumbnailPath);
-        //Foreach image file in $filepath...
-        chdir($filepath);
-        $files = glob('*.{jpg,jpeg,gif}', GLOB_BRACE);
-        if(sizeof($files) > 0) {
-            foreach($files as $file) {
-                
-                $cloudinary_options = array("background"=>"black", "crop"=>"pad", "width"=>0, "height"=>0);
-                //If 200 thumbnail doesn't already exist
-                if (!file_exists($thumbnailPath . '/thumb200_' . $file)) {
-                    //create thumbnail
-                    $cloudinary_options["width"] = 200;
-                    $cloudinary_options["height"] = 134;
-                    $upload = \Cloudinary\Uploader::upload($file, $cloudinary_options);
 
-                    //Suffix thumbnail filename with thumb200_ and place in thumbnail subdirectory
-                    if (file_put_contents($thumbnailPath . '/thumb200_' . $file, file_get_contents($upload["url"])) === false) {
-                        throw new Exception("Could not put thumbnail in correct directory.");
-                    }
+
+        
+        //Database query all booth uploaded photos with event ID as above
+
+        if() { //Query is not empty
+
+            //For each file, if the logoified file doesn't already exist...
+            if (!file_exists($logoPath . '/logo_' . $file)) {
+                //create it...
+                $upload = \Cloudinary\Uploader::upload($file, array('public_id' => $photoID, 'transformation' => array(
+                    'overlay' => 'lrpLogo',
+                    'width' => 200,
+                    'gravity' => 'south_east',
+                    
+                )));
+
+                //...and prefix filename with logo_ and place in thumbnail subdirectory
+                if (file_put_contents($logoPath . '/logo_' . $file, file_get_contents($upload["url"])) === false) {
+                    throw new Exception("Could not put thumbnail in correct directory.");
                 }
-
-                //If 500 thumbnail doesn't already exist
-                if (!file_exists($thumbnailPath . '/thumb500_' . $file)) {
-                    //create thumbnail
-                    $cloudinary_options["width"] = 500;
-                    $cloudinary_options["height"] = 334;
-                    $upload = \Cloudinary\Uploader::upload($file, $cloudinary_options);
-
-                    //Suffix thumbnail filename with thumb500_ and place in thumbnail subdirectory
-                    if (file_put_contents($thumbnailPath . '/thumb500_' . $file, file_get_contents($upload["url"])) === false) {
-                        throw new Exception("Could not put thumbnail in correct directory.");
-                    }
-                }
-                
-
             }
-        } else {
-            throw new Exception('No images found in {$filepath}');
         }
-        
-        
     }
 
-    detectAndCreateThumbnails(1);
+    addLogoByEventId(1);
     
 ?>
