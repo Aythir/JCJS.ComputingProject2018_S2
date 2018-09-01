51d045bf25aac1c898f1fe9eddd57f79b1193f3f
clea1 <39670057+clea1@users.noreply.github.com>
2018-09-01 12:24:35 +1000

    Display 3 images on mobile

--- JCJS.PrototypeWebsite/gallery.php
+++ JCJS.PrototypeWebsite/gallery.php
@@ -99,7 +99,7 @@ include 'functionList.php';
             <button id="create" type="button" class="btn btn-default" onclick="enableSelector();">Create Animation</button>
         </div> 
         <br>       
-        <div class= "row">
+        <div class= "row" style="padding:0 0 0 0">
             <?php
                 $sql = "SELECT PhotoID,Filename FROM Photos WHERE EventID = '$eventID' AND IsUserUpload = 1;";
                 //echo $sql;
@@ -108,8 +108,8 @@ include 'functionList.php';
                 if ($result->num_rows > 0) {
                     // output data of each row
                     while($row = $result->fetch_assoc()) {
-                        //echo '<div class= "col-lg-3 col-sm-6" onclick="location.href=\'showPhoto.php?PhotoID='.$row["PhotoID"].'\'" style="cursor:pointer;">';
-                        echo '<div class= "col-lg-3 col-sm-6" style="cursor:pointer;">';
+                        //echo '<div class= "col-4 col-lg-3 col-sm-4" style="padding: 0" onclick="location.href=\'showPhoto.php?PhotoID='.$row["PhotoID"].'\'" style="cursor:pointer;">';
+                        echo '<div class= " col-4 col-lg-3 col-sm-4" style="cursor:pointer; padding:0">';
                         echo '<div id="gallery" class="card">';
                         echo '<img src="eventPhotos/'.$eventID.'/'.$row["Filename"].'" class="card-img-top" id="'.$row["PhotoID"].'" style="border:1px solid white">';
                         echo "</div>";
@@ -119,6 +119,7 @@ include 'functionList.php';
             ?>
         </div>
     </div>
+   </div>
     <!-- Public event photo gallery-->
     <div class="user-gallery tz-gallery">
         <h3>Public Event Photos: <?php echo $eventName ?></h3>
@@ -132,8 +133,8 @@ include 'functionList.php';
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    //echo '<div class= "col-lg-3 col-sm-6" onclick="location.href=\'showPhoto.php?PhotoID='.$row["PhotoID"].'\'" style="cursor:pointer;">';
-                    echo '<div class= "col-lg-3 col-sm-6" style="cursor:pointer;">';
+                    //echo '<div class= "col-4 col-lg-3 col-sm-4" style="padding: 0" onclick="location.href=\'showPhoto.php?PhotoID='.$row["PhotoID"].'\'" style="cursor:pointer;">';
+                    echo '<div class= "col-4 col-lg-3 col-sm-4 " style="cursor:pointer; padding:0">';
                     echo '<div id="gallery" class="card">';
                     echo '<img src="eventPhotos/'.$eventID.'/'.$row["Filename"].'" class="card-img-top" id="'.$row["PhotoID"].'" style="border:1px solid white">';
                     echo "</div>";
