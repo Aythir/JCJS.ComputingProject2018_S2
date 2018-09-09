dcfd879de267e18ac8bd67f774ea834f94675c77
Steve Daniel <steve@posmarket.com.au>
2018-09-09 14:25:00 +1000

    Hide header from Slideshow

--- JCJS.PrototypeWebsite/slideshow.php
+++ JCJS.PrototypeWebsite/slideshow.php
@@ -12,94 +12,70 @@
     }  
 ?>
 <?php include 'guestHeader.php';?>
-  <!-- Content -->
-   <div class="container-liquid">
-        <!--Grid row-->
-        <div class="row" style="margin-top:80px">
-          <!--Grid column-->
-         <div class="col black-text text-center">
-
-         </div>
-       </div>
-
-   <!--Carousel-->
-
-   <div class="container mt-3">
-         <div id="myCarousel" class="carousel slide">
-
-        <!-- Indicators -->
-        <ul class="carousel-indicators">
-          <li class="item1 active"></li>
-          <li class="item2"></li>
-          <li class="item3"></li>
-        </ul>
-
+<!--Carousel-->
+<div class="container">
+    <div id="myCarousel" class="carousel slide">
         <div class="carousel-inner">
-        <!-- The slideshow -->
-        <?php
-        $sql = "SELECT FileName FROM `photos` WHERE eventID = ".$eventID." AND IsUserUpload = 1;";
-        //echo $sql;
-        $result = $conn->query($sql);
+            <!-- The slideshow -->
+            <?php
+            $sql = "SELECT FileName FROM `photos` WHERE eventID = ".$eventID." AND IsUserUpload = 1;";
+            //echo $sql;
+            $result = $conn->query($sql);
 
-        $idx = 0;
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                if($idx == 0) {
-                    echo '<div class="carousel-item active">'."\r\n";
+            $idx = 0;
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    if($idx == 0) {
+                        echo '<div class="carousel-item active">'."\r\n";
+                    }
+                    else {
+                        echo '<div class="carousel-item">'."\r\n";    
+                    }
+                    echo "<img src='eventPhotos/".$eventID."/".$row["FileName"]."' alt='photo' width='100%'>"."\r\n";
+                    echo "</div>"."\r\n";
+                    $idx++;
                 }
-                else {
-                    echo '<div class="carousel-item">'."\r\n";    
-                }
-                echo "<img src='eventPhotos/".$eventID."/".$row["FileName"]."' alt='photo' width='100%'>"."\r\n";
-                echo "</div>"."\r\n";
-                $idx++;
             }
-        }
-        ?>
+            ?>
         </div>
-
         <!-- Left and right controls -->
-        <a class="carousel-control-prev" href="#myCarousel" >
-          <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel">
-          <span class="carousel-control-next-icon"></span>
-        </a>
-
-      </div>
-   </div>
-
-   <script>
-   $(document).ready(function(){
-       // Activate Carousel
-       $("#myCarousel").carousel();
-
-       // Enable Carousel Indicators
-       $(".item1").click(function(){
-           $("#myCarousel").carousel(0);
-       });
-       $(".item2").click(function(){
-           $("#myCarousel").carousel(1);
-       });
-       $(".item3").click(function(){
-           $("#myCarousel").carousel(2);
-       });
-
-       // Enable Carousel Controls
-       $(".carousel-control-prev").click(function(){
-           $("#myCarousel").carousel("prev");
-       });
-       $(".carousel-control-next").click(function(){
-           $("#myCarousel").carousel("next");
-       });
-   });
-
-   // refresh page every 1 minute
-    setTimeout(function() {
-        location.reload();
-    }, 100000);
-   </script>
-
+        <a class="carousel-control-prev" href="#myCarousel" ><span class="carousel-control-prev-icon"></span></a>
+        <a class="carousel-control-next" href="#myCarousel"><span class="carousel-control-next-icon"></span></a>
+    </div>
 </div>
+<script>
+    setTimeout(function() {
+        document.getElementById("mainHeader").style.top = "-90px";
+        document.getElementById("myCarousel").style.top = "-150px";        
+    }, 3000);
+
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+
+    // Enable Carousel Controls
+    $(".carousel-control-prev").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".carousel-control-next").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+
+// refresh page every 1 minute
+setTimeout(function() {
+    location.reload();
+}, 100000);
+</script>
 </body>
