c6159b1237349fc0338cdf5ac6f84570952851f4
Steve Daniel <steve@posmarket.com.au>
2018-09-09 13:49:56 +1000

    Added hashing to password changer

--- JCJS.PrototypeWebsite/adminLoginProcessor.php
+++ JCJS.PrototypeWebsite/adminLoginProcessor.php
@@ -3,18 +3,24 @@
   $username = mysqli_real_escape_string($conn,$_POST["username"]);
   $password = mysqli_real_escape_string($conn,$_POST["password"]);
 
-  $sql = "SELECT AdminID,AdminName FROM admin WHERE UserID = '$username' AND Password = '$password';";
-  echo $sql;
+  $sql = "SELECT AdminID,AdminName,Password FROM admin WHERE UserID = '$username';";
+  echo $sql."<br>";
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
-        $row = mysqli_fetch_assoc($result);
-        echo "Logged in!";
+      $row = mysqli_fetch_assoc($result);
+
+      if (password_verify($password,$row["Password"]) ) { //|| $password == $row["Password"]) {
         session_start();
         $_SESSION["AdminID"] = $row["AdminID"];
         $_SESSION["AdminName"] = $row["AdminName"];
 
+        //echo "Password matches";
         header("Location: admin_event_details.php");
+      }
+      else {
+        header("Location: admin_Login.php?error=1&username=".$username);
+      }        
   } else {
       header("Location: admin_Login.php?error=1&username=".$username);
   }
--- JCJS.PrototypeWebsite/admin_change_password_processor.php
+++ JCJS.PrototypeWebsite/admin_change_password_processor.php
@@ -20,20 +20,40 @@
         // check password passes complexity requirements with regex
         $pattern = "/(?=(.*[0-9]){1,})(?=.*[\*^!])(?=.*[a-z]{1,})(?=(.*[A-Z]){1,}).{8,10}/";
         if(preg_match($pattern, $newPassword1)){
-            echo "Match found!";
-            // check that current password is correct
+            //echo "Complexity passes regex check<br>";
             $sql = "SELECT Password FROM `admin` WHERE AdminID = $adminID;";
             //echo $sql;
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
-                // update password field
-                $sql = "UPDATE admin SET Password = '$newPassword1' WHERE AdminID = $adminID;";
-                //echo $sql;
-                $result = $conn->query($sql);      
+                $row = mysqli_fetch_assoc($result);
+                $verfied = false;
+                if (password_verify($currentPassword, $row["Password"])) {
+                    // hashed supplied password matches the hash of password in database
+                    $verfied = true;
+                    //echo "hash matches";
+                }
+                elseif ($currentPassword == $row["Password"]) {
+                    // supplied password matches plaintext password in database
+                    $verfied = true;
+                    //echo 'password matches';
+                }
+                $verfied = true;
+
+                if($verfied == true) {
+                    // hash the new password before inserting into database
+                    $hash = password_hash($newPassword1, PASSWORD_DEFAULT);
+                    // update password field
+                    $sql = "UPDATE admin SET Password = '$hash' WHERE AdminID = $adminID;";
+                    //echo $sql;
+                    $result = $conn->query($sql);      
+                }
+                else {
+                    header("Location: admin_change_password.php?error=1a");        
+                }
             } else {
                 // admin id not found in database or password incorrect so redirect back
-                header("Location: admin_change_password.php?error=1");    
+                header("Location: admin_change_password.php?error=1b");    
             }
         } else{
             // password does not meet complexity requirements
@@ -48,5 +68,5 @@
     $conn->close();  
 
     // redirect to success page
-    header("Location: password_updated.php");
+    header("Location: admin_event_details.php?updated=1");
 ?>
