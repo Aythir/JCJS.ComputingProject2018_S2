cc89417d0ca6dca72f524320da4bd135bfcaab7f
Jackson Meyn <jacksonmeyn@gmail.com>
2018-09-21 14:15:30 +1000

    Add check for duplicate codes when updating event details

--- JCJS.PrototypeWebsite/eventProcessor.php
+++ JCJS.PrototypeWebsite/eventProcessor.php
@@ -9,12 +9,26 @@
     $hostAccessCode = mysqli_real_escape_string($conn,$_POST["hostAccessCode"]);
 
     if($eventID > 0) {
-        $sql = "UPDATE Events SET eventDate = '$eventDate',eventName = '$eventName',eventLocation = '$eventLocation',guestAccessCode = '$guestAccessCode',hostAccessCode = '$hostAccessCode' WHERE eventID = $eventID;";
+        //Check for duplicate before update
+        $sql = "SELECT eventName FROM Events WHERE NOT eventID = $eventID AND (guestAccessCode = '$guestAccessCode' OR hostAccessCode = '$hostAccessCode');";
         //echo $sql;
-        $result = $conn->query($sql);      
+        $result = $conn->query($sql);
 
-        $conn->close();  
-        header("Location: admin_event_details.php?saved=".$eventID);
+        if ($result->num_rows > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $eventName = $row["eventName"];
+            //header("Location: admin_event_details.php?duplicate=".$eventName);
+            echo "Duplicate host and/or guest code(s). Click Back and make sure they're not the same as another event already in the database.";
+        } else {
+            $sql = "UPDATE Events SET eventDate = '$eventDate',eventName = '$eventName',eventLocation = '$eventLocation',guestAccessCode = '$guestAccessCode',hostAccessCode = '$hostAccessCode' WHERE eventID = $eventID;";
+            //echo $sql;
+            $result = $conn->query($sql);      
+
+            $conn->close();  
+            header("Location: admin_event_details.php?saved=".$eventID);
+        }
+        
+        
     } else {
         $sql = "SELECT eventName FROM Events WHERE guestAccessCode = '$guestAccessCode' OR hostAccessCode = '$hostAccessCode';";
         //echo $sql;
@@ -24,7 +38,7 @@
             $row = mysqli_fetch_assoc($result);
             $eventName = $row["eventName"];
             //header("Location: admin_event_details.php?duplicate=".$eventName);
-            echo "Duplicate host or guest code(s).";
+            echo "Duplicate host and/or guest code(s). Click Back and make sure they're not the same as another event already in the database.";
         } else {
             $sql = "INSERT INTO Events (eventDate,eventName,eventLocation,guestAccessCode,hostAccessCode) VALUES ('$eventDate','$eventName','$eventLocation','$guestAccessCode','$hostAccessCode');";
             //echo $sql;
